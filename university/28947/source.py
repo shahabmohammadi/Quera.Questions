18d61de80a1534d59fcb5a715751a5fcd58fe114
Shahabaldin Mohammadi <45038855+stevelex-elex@users.noreply.github.com>
2019-06-26 16:23:59 +0430

    university 28947 question passed

---
+++ university/28947/source.py
@@ -0,0 +1,14 @@
+my_list = [
+    "sir",
+    "sib",
+    "samano",
+    "senjed",
+    "serke",
+    "seke",
+    "somagh",
+    "sonbol",
+    "sabze"
+]
+target = int(input())
+for i in range(target):
+    print(my_list[i])
